f25a1e23c0a53d1cf4f80a43a776b8282b424c11
Gustav Louw <yellingintothefan@gmail.com>
2018-02-21 11:43:10 -0800

    fixed a couple mistakes learned from andvaranaut

--- main.c
+++ main.c
@@ -106,33 +106,24 @@ static char tile(const Point a, const char** const tiles)
 typedef struct
 {
     char tile;
-    float offset;
     Point where;
 }
 Hit;
 
-// Returns the decimal portion of a float.
 static float dec(const float x)
 {
     return x - (int) x;
 }
 
-static Hit collision(const Point hook, const Point direction, const char** const walling)
+static Hit cast(const Point where, const Point direction, const char** const walling)
 {
-    const float epsilon = 1e-4;
-    const Hit hit = {
-        tile(add(hook, mul(direction, epsilon)), walling),
-        dec(hook.x) + dec(hook.y),
-        hook
-    };
-    return hit;
-}
-
-static Hit cast(const Point ray, const Point direction, const char** const walling)
-{
-    const Point hook = cmp(ray, sh(ray, direction), sv(ray, direction));
-    const Hit hit = collision(hook, direction, walling);
-    return hit.tile ? hit : cast(hook, direction, walling);
+    const Point ray = cmp(where, sh(where, direction), sv(where, direction));
+    const Point delta = mul(direction, 0.01);
+    const Point dx = { delta.x, 0.0 };
+    const Point dy = { 0.0, delta.y };
+    const Point test = add(ray, dec(ray.x) == 0.0 ? dx : dec(ray.y) == 0.0 ? dy : delta);
+    const Hit hit = { tile(test, walling), ray };
+    return hit.tile ? hit : cast(ray, direction, walling);
 }
 
 typedef struct
@@ -174,6 +165,7 @@ typedef struct
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* texture;
+    int res;
 }
 Gpu;
 
@@ -183,7 +175,7 @@ static Gpu setup(const int res)
     SDL_Window* const window = SDL_CreateWindow("water", 0, 0, res, res, SDL_WINDOW_SHOWN);
     SDL_Renderer* const renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Texture* const texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, res, res);
-    const Gpu gpu = { window, renderer, texture };
+    const Gpu gpu = { window, renderer, texture, res };
     return gpu;
 }
 
@@ -264,10 +256,8 @@ static Hero spin(const Hero hero)
     return step;
 }
 
-static Hero move(const Hero hero, const char** const walling)
+static Hero move(const Hero hero, const char** const walling, const uint8_t* key)
 {
-    const uint8_t* key = SDL_GetKeyboardState(NULL);
-    SDL_PumpEvents();
     Hero step = hero;
     // Accelerates if <WASD>.
     if(key[SDL_SCANCODE_W] || key[SDL_SCANCODE_S] || key[SDL_SCANCODE_D] || key[SDL_SCANCODE_A])
@@ -305,25 +295,25 @@ static uint32_t color(const char tile)
     return 0xAA << (8 * (tile - 1));
 }
 
-static void render(const Hero hero, const Map map, const int res, const Gpu gpu)
+static void render(const Hero hero, const Map map, const Gpu gpu)
 {
     const int t0 = SDL_GetTicks();
     const Line camera = rotate(hero.fov, hero.theta);
     const Display display = lock(gpu);
-    for(int x = 0; x < res; x++)
+    for(int x = 0; x < gpu.res; x++)
     {
         // Casts a ray.
-        const Point column = lerp(camera, x / (float) res);
+        const Point column = lerp(camera, x / (float) gpu.res);
         const Hit hit = cast(hero.where, column, map.walling);
         const Point ray = sub(hit.where, hero.where);
         const Point corrected = turn(ray, -hero.theta);
-        const Wall wall = project(res, hero.fov, corrected);
+        const Wall wall = project(gpu.res, hero.fov, corrected);
         const Line trace = { hero.where, hit.where };
-        // Renders ceiling. 
+        // Renders ceiling.
         for(int y = 0; y < wall.top; y++)
             // Subtracts one from y to prevent overcast edge cases.
             put(display, x, y, color(
-                tile(lerp(trace, ccast(hero.fov, res, y - 1) / corrected.x), map.ceiling)
+                tile(lerp(trace, ccast(hero.fov, gpu.res, y + 1) / corrected.x), map.ceiling)
             ));
         // Renders wall.
         for(int y = wall.top; y < wall.bot; y++)
@@ -331,10 +321,10 @@ static void render(const Hero hero, const Map map, const int res, const Gpu gpu)
                 hit.tile
             ));
         // Renders flooring.
-        for(int y = wall.bot; y < res; y++)
+        for(int y = wall.bot; y < gpu.res; y++)
             // Adds one to y to prevent overcast edge cases.
             put(display, x, y, color(
-                tile(lerp(trace, fcast(hero.fov, res, y + 1) / corrected.x), map.flooring)
+                tile(lerp(trace, fcast(hero.fov, gpu.res, y + 0) / corrected.x), map.flooring)
             ));
     }
     unlock(gpu);
@@ -349,41 +339,42 @@ static int finished()
 {
     SDL_Event event;
     SDL_PollEvent(&event);
-    if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_END)
+    if(event.type == SDL_QUIT
+    || event.key.keysym.sym == SDLK_END
+    || event.key.keysym.sym == SDLK_ESCAPE)
         return 1;
     return 0;
 }
 
 int main()
 {
-    const int res = 500;
-    const Gpu gpu = setup(res);
+    const Gpu gpu = setup(500);
     const char* ceiling[] = {
-        "1111111111111111",
-        "1222232232322321",
-        "122222221111232111111111111111",
-        "122221221232323232323232323231",
-        "122222221111232111111111111111",
-        "1222232232322321",
-        "1111111111111111",
+        "1111111111111111             1111111111111111",
+        "1222232232322321             1222232232322321",
+        "122222221111232111111111111111222222211112321",
+        "122221221232323232323232323232222212212323231",
+        "122222221111232111111111111111222222211112321",
+        "1222232232322321             1222232232322321",
+        "1111111111111111             1111111111111111",
     };
     const char* walling[] = {
-        "1111111111111111",
-        "1000000000000001",
-        "103330001111000111111111111111",
-        "103000003000020000000000000001",
-        "103330001111000111111111111111",
-        "1000000000000001",
-        "1111111111111111",
+        "1111111111111111             1111111111111111",
+        "1000000000000001             1000000000000001",
+        "103330001111000111111111111111033300011110001",
+        "103000000000000000000000000000030000030000001",
+        "103330001111000111111111111111033300011110001",
+        "1000000000000001             1000000000000001",
+        "1111111111111111             1111111111111111",
     };
     const char* flooring[] = {
-        "1111111111111111",
-        "1222232232322321",
-        "122222221111232111111111111111",
-        "122222221232323323232323232321",
-        "122222221111232111111111111111",
-        "1222232232322321",
-        "1111111111111111",
+        "1111111111111111             1111111111111111",
+        "1222232232322321             1222232232322321",
+        "122222221111232111111111111111222222211112321",
+        "122222221232323323232323232323222222212323231",
+        "122222221111232111111111111111222222211112321",
+        "1222232232322321             1222232232322321",
+        "1111111111111111             1111111111111111",
     };
     const Map map = { ceiling, walling, flooring };
     Hero hero = {
@@ -402,8 +393,9 @@ int main()
     };
     while(!finished())
     {
-        hero = spin(move(hero, walling));
-        render(hero, map, res, gpu);
+        const uint8_t* key = SDL_GetKeyboardState(NULL);
+        hero = spin(move(hero, walling, key));
+        render(hero, map, gpu);
     }
     release(gpu);
 }
