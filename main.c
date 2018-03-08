6a45e5043e515315b18fb7a3353c84118dc12e61
Gustav Louw <yellingintothefan@gmail.com>
2018-03-08 13:43:36 -0800

    widescreen support

--- main.c
+++ main.c
@@ -152,9 +152,9 @@ typedef struct
 Line;
 
 // Party casting (flooring and ceiling)
-static float pcast(const float size, const int res, const int y)
+static float pcast(const float size, const int yres, const int y)
 {
-    return size / (2 * (y + 1) - res);
+    return size / (2 * (y + 1) - yres);
 }
 
 static Line rotate(const Line l, const float t)
@@ -177,17 +177,19 @@ typedef struct
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* texture;
-    int res;
+    int xres;
+    int yres;
 }
 Gpu;
 
-static Gpu setup(const int res)
+static Gpu setup(const int xres, const int yres)
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* const window = SDL_CreateWindow("littlewolf", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, res, res, SDL_WINDOW_SHOWN);
+    SDL_Window* const window = SDL_CreateWindow("littlewolf", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, xres, yres, SDL_WINDOW_SHOWN);
     SDL_Renderer* const renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture* const texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, res, res);
-    const Gpu gpu = { window, renderer, texture, res };
+    // Notice the flip between xres and yres. The texture is 90 degrees flipped on its side for fast cache access.
+    SDL_Texture* const texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, yres, xres);
+    const Gpu gpu = { window, renderer, texture, xres, yres };
     return gpu;
 }
 
@@ -201,7 +203,13 @@ static void release(const Gpu gpu)
 
 static void present(const Gpu gpu)
 {
-    SDL_RenderCopyEx(gpu.renderer, gpu.texture, NULL, NULL, -90.0, NULL, SDL_FLIP_NONE);
+    const SDL_Rect dst = {
+        (gpu.xres - gpu.yres) / 2,
+        (gpu.yres - gpu.xres) / 2,
+        gpu.yres,
+        gpu.xres,
+    };
+    SDL_RenderCopyEx(gpu.renderer, gpu.texture, NULL, &dst, -90, NULL, SDL_FLIP_NONE);
     SDL_RenderPresent(gpu.renderer);
 }
 
@@ -239,12 +247,12 @@ typedef struct
 }
 Wall;
 
-static Wall project(const int res, const Line fov, const Point corrected)
+static Wall project(const int xres, const int yres, const Line fov, const Point corrected)
 {
-    const float size = 0.5f * fov.a.x * res / corrected.x;
-    const int top = (res + size) / 2.0f;
-    const int bot = (res - size) / 2.0f;
-    const Wall wall = { top > res ? res : top, bot < 0 ? 0 : bot, size };
+    const float size = 0.5f * fov.a.x * xres / corrected.x;
+    const int top = (yres + size) / 2.0f;
+    const int bot = (yres - size) / 2.0f;
+    const Wall wall = { top > yres ? yres : top, bot < 0 ? 0 : bot, size };
     return wall;
 }
 
@@ -313,24 +321,24 @@ static void render(const Hero hero, const Map map, const Gpu gpu)
     const int t0 = SDL_GetTicks();
     const Line camera = rotate(hero.fov, hero.theta);
     const Display display = lock(gpu);
-    for(int x = 0; x < gpu.res; x++)
+    for(int x = 0; x < gpu.xres; x++)
     {
         // Casts a ray.
-        const Point column = lerp(camera, x / (float) gpu.res);
+        const Point column = lerp(camera, x / (float) gpu.xres);
         const Hit hit = cast(hero.where, column, map.walling);
         const Point ray = sub(hit.where, hero.where);
         const Point corrected = turn(ray, -hero.theta);
-        const Wall wall = project(gpu.res, hero.fov, corrected);
+        const Wall wall = project(gpu.xres, gpu.yres, hero.fov, corrected);
         const Line trace = { hero.where, hit.where };
         // Renders flooring.
         for(int y = 0; y < wall.bot; y++)
-            put(display, x, y, color(tile(lerp(trace, -pcast(wall.size, gpu.res, y)), map.floring)));
+            put(display, x, y, color(tile(lerp(trace, -pcast(wall.size, gpu.yres, y)), map.floring)));
         // Renders wall.
         for(int y = wall.bot; y < wall.top; y++)
             put(display, x, y, color(hit.tile));
         // Renders ceiling.
-        for(int y = wall.top; y < gpu.res; y++)
-            put(display, x, y, color(tile(lerp(trace, +pcast(wall.size, gpu.res, y)), map.ceiling)));
+        for(int y = wall.top; y < gpu.xres; y++)
+            put(display, x, y, color(tile(lerp(trace, +pcast(wall.size, gpu.yres, y)), map.ceiling)));
     }
     unlock(gpu);
     present(gpu);
@@ -349,13 +357,21 @@ static int done()
         || event.key.keysym.sym == SDLK_ESCAPE;
 }
 
+static Line viewport(const float focal)
+{
+    const Line fov = {
+        { focal, -1.0f },
+        { focal, +1.0f },
+    };
+    return fov;
+}
+
 static Hero born()
 {
     const Hero hero = {
-        // Field of View.
-        { { +1.0f, -1.0f }, { +1.0f, +1.0f } },
-        // Where..
-        { 3.5f, 3.5f },
+        viewport(0.8f),
+        // Where.
+        { 4.5f, 3.5f },
         // Velocity.
         { 0.0f, 0.0f },
         // Speed.
@@ -398,11 +414,7 @@ static Map build()
         "122223223232232111111111111111222232232322321",
         "111111111111111111111111111111111111111111111",
     };
-    const Map map = {
-        ceiling,
-        walling,
-        floring,
-    };
+    const Map map = { ceiling, walling, floring };
     return map;
 }
 
@@ -410,7 +422,7 @@ int main(int argc, char* argv[])
 {
     (void) argc;
     (void) argv;
-    const Gpu gpu = setup(500);
+    const Gpu gpu = setup(800, 500);
     const Map map = build();
     Hero hero = born();
     while(!done())
